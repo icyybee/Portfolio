41f0cb4bc2de15031f896eb3dcd96e55a3042903
Beatrice Egumandi <ushiksmandi@gmail.com>
2022-07-26 19:49:38 +0100

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let bio__links = document.getElementsByClassName('bio__links');
-let bio__contents = document.getElementsByClassName('bio__contents');
+var bio__links = document.getElementsByClassName('bio__links');
+var bio__contents = document.getElementsByClassName('bio__contents');
 function opentab(tabname) {
     for(bio__link of bio__links) {
-        bio__link.classList.remove("head-active");
+        bio__link.classList.remove('head-active');
     }
     for(bio__content of bio__contents) {
-        bio__content.classList.remove("content-active");
+        bio__content.classList.remove('content-active');
     }
-    event.currentTarget.classList.add("content-active");
-    document.getElementById(tabname).classList.add("head-active");
+    event.currentTarget.classList.add('head-active');
+    document.getElementById(tabname).classList.add('content-active');
 }
 
 $(document).ready(function(){
@@ -24,3 +24,13 @@ $(document).ready(function(){
         offset: '60px;'
     });
 });
+
+/*sidemenu*/
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-20rem";
+}
